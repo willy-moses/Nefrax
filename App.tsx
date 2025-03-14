6b303d6bb06d1f561d8cd00f7dd08d93e96ca4c1
willy-moses <willkapenamowa@gmail.com>
2025-03-14 22:22:04 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUpScreen from './screens/SignUpScreen';
+import SignInScreen from './screens/SignInScreen';
 
-export default function App() {
+import { RootStackParamList } from './types';
+
+
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignUp" component={SignUpScreen}  options={{ headerShown: false }} 
+        
+ />
+ 
+        <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }}  />
+      
+        
+        {/* Add other screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ types.tsx
@@ -0,0 +1,9 @@
+
+  export type RootStackParamList = {
+   
+    Profile: undefined; // ✅ Ensure "Profile" is listed here
+    SignUp: undefined;  // You can add params if you want to pass them
+    SignIn: undefined;
+  };
+  
+  

f3981f694e276028c382a4d396593cac53186453
willy-moses <willkapenamowa@gmail.com>
2025-03-21 05:49:14 +0200

    Update .gitignore file

--- types.tsx
+++
@@ -1,9 +0,0 @@
-
-  export type RootStackParamList = {
-   
-    Profile: undefined; // ✅ Ensure "Profile" is listed here
-    SignUp: undefined;  // You can add params if you want to pass them
-    SignIn: undefined;
-  };
-  
-  
